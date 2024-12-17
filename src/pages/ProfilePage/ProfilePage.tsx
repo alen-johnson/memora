2abfd6519ae4f876e17f693bc1cddfd0a2871516
Alen J Ninan <alen.j@captainfresh.in>
2024-12-17 19:00:04 +0530

    profile ui customization

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,20 +1,20 @@
-import { ArrowLeftOutlined, PlusOutlined } from "@ant-design/icons";
-import "./ProfilePage.css";
+import './ProfilePage.css'
+import { PlusOutlined } from "@ant-design/icons";
 import { Button, FloatButton, Modal } from "antd";
 import { useNavigate } from "react-router-dom";
 import useLogout from "../../hooks/useLogout";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../services/firebase";
 import useAuthStore from "../../store/authStore";
-import { profile } from "../../assets/imageIndex";
+import {ProfileHeader} from "../../components/componetIndex";  
 
 function ProfilePage() {
   const navigate = useNavigate();
   const { handleLogout, isLoggingOut } = useLogout();
   const [isOpen, setIsOpen] = useState(false);
   const [user] = useAuthState(auth);
-  const authUser = useAuthStore((state) => state.user)
+  const authUser = useAuthStore((state) => state.user);
 
   const handleLogoutAndNavigate = async () => {
     await handleLogout();
@@ -24,61 +24,29 @@ function ProfilePage() {
   const arrowClick = () => {
     navigate("/");
   };
-
-  const profileimg = authUser?.profilePicURL && authUser.profilePicURL !== "" 
-  ? authUser.profilePicURL 
-  : profile;
+    useEffect(() => {
+      console.log(authUser)
+    },[authUser])
 
   return (
     <div className="profile">
-      <div className="profile__header">
-        <ArrowLeftOutlined onClick={arrowClick} />
-        
-        {/* Conditional rendering for Login/Signup or Logout button */}
-        {!user ? (
-          <Button
-            className="profile__header-btn1"
-            type="primary"
-            onClick={arrowClick}
-          >
-            Login/Signup
-          </Button>
-        ) : (
-          <Button
-            className="profile__header-logoutbtn"
-            danger
-            onClick={() => setIsOpen(!isOpen)}
-            loading={isLoggingOut}
-          >
-            Logout
-          </Button>
-        )}
-
-        <img src={authUser?.coverPicUrl} alt="cover" className="profile__header-cover" />
-        <img src={profileimg} alt="Profile" className="profile__header-dp" />
-
-        {user && (
-          <Button className="profile__header-btn2">Edit Profile</Button>
-        )}
-
-        <div className="profile__friends">
-          <h3>Friends List</h3>
-          <ul>
-            <li>John Doe</li>
-            <li>Mary Smith</li>
-            <li>Robert Brown</li>
-            <li>Emily Johnson</li>
-          </ul>
-        </div>
+      <ProfileHeader
+        text="Go Back"
+        showButtons={user !== null} // Conditionally render buttons based on user state
+        onArrowClick={arrowClick}
+        onLogoutClick={() => setIsOpen(!isOpen)}
+        isLoggingOut={isLoggingOut}
+      />
+      <div className='profile__head'>
+      <Button  className='profile__head-edtbtn'>Edit Profile</Button>
       </div>
       <div className="profile__bio">
         <h2>{authUser?.fullname}</h2>
-        <p>
-{authUser?.bio}
-        </p>
+        <p>{authUser?.bio}</p>
       </div>
       <div className="profile__posts">
         <h4>My Posts</h4>
+
         <Modal
           open={isOpen}
           onOk={handleLogoutAndNavigate}
