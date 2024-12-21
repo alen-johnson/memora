af422ce8fc4a24a5d7fd9b9d4853fd2d42db4d17
Alen J Ninan <alen.j@captainfresh.in>
2024-12-21 15:00:01 +0530

    added capture photo feature

--- src/pages/PostPage/PostPage.tsx
+++ src/pages/PostPage/PostPage.tsx
@@ -7,14 +7,17 @@ import useShowMessage from "../../hooks/useShowMessage";
 import useCreatePost from "../../hooks/useCreatePost";
 import { useNavigate } from "react-router-dom";
 import { ArrowLeftOutlined } from "@ant-design/icons";
+import { useCameraCapture } from "../../hooks/useCamerCapture";
 
 function PostPage() {
   const [fileUrls, setFileUrls] = useState<string[]>([]);
   const [files, setFiles] = useState<FileWithPath[]>([]);
   const [caption, setCaption] = useState<string>("");
+  const [isCameraActive, setIsCameraActive] = useState<boolean>(false);
   const videoRef = useRef<HTMLVideoElement | null>(null);
   const navigate = useNavigate();
 
+  const { capturedImage, startCamera, clearCapturedImage } = useCameraCapture();
   const { isLoading, handleCreatePost } = useCreatePost();
   const { showError } = useShowMessage();
 
@@ -61,17 +64,18 @@ function PostPage() {
     setFiles((prevFiles) => prevFiles.filter((_, i) => i !== index));
     setFileUrls((prevUrls) => prevUrls.filter((_, i) => i !== index));
   };
+
   const arrowClick = () => {
     navigate("/");
   };
 
   const handlePost = async () => {
-    if (files.length === 0) {
+    if (files.length === 0 && !capturedImage) {
       showError("Please select an image to upload.");
       return;
     }
 
-    const selectedFile = files[0];
+    const selectedFile = files[0] || capturedImage;
     const reader = new FileReader();
 
     reader.onload = async () => {
@@ -88,6 +92,27 @@ function PostPage() {
     reader.readAsDataURL(selectedFile);
   };
 
+  const handleCameraClick = async () => {
+    await startCamera(); 
+    setIsCameraActive(true);
+  
+    if (capturedImage) {
+      const imgContainer = document.querySelector(".post__img-container");
+      const previewContainer = document.querySelector(".post__preview-container");
+  
+      if (imgContainer && previewContainer) {
+        imgContainer.classList.add("hidden");
+        previewContainer.classList.remove("hidden"); 
+      }
+    }
+  };
+  
+
+  const handleClearImage = () => {
+    clearCapturedImage();
+    setIsCameraActive(false); 
+  };
+
   return (
     <div className="post">
       <ArrowLeftOutlined
@@ -98,12 +123,24 @@ function PostPage() {
 
       <h4>Create Post</h4>
 
+      <div>
+        {capturedImage ? (
+          <div>
+            <button className="remove-file-btn" onClick={handleClearImage}>
+              &times;
+            </button>
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
+
       <div
-        className={`post__img-container ${fileUrls.length > 0 ? "hidden" : ""}`}
+        className={`post__img-container ${fileUrls.length > 0 || capturedImage || isCameraActive ? "hidden" : ""}`}
         {...getRootProps()}
       >
         <input id="file-input" {...getInputProps()} />
-        {fileUrls.length === 0 && (
+        {fileUrls.length === 0 && !capturedImage && !isCameraActive && (
           <div className="post__img-box">
             <img src={post} className="post-img" alt="Default" />
             <h3 className="post-drag">Drag photo or Video here</h3>
@@ -114,51 +151,67 @@ function PostPage() {
 
       <div
         className={`post__preview-container ${
-          fileUrls.length > 0 ? "expanded" : "hidden"
+          fileUrls.length > 0 || capturedImage ? "expanded" : "hidden"
         }`}
       >
-        {fileUrls.map((url, index) => (
-          <div key={index} className="post__preview-item">
-            <button
-              className="remove-file-btn"
-              onClick={() => removeFile(index)}
-            >
-              &times;
-            </button>
-            {files[index]?.type.startsWith("image") ? (
-              <img src={url} className="post-preview" alt="Preview" />
-            ) : files[index]?.type.startsWith("video") ? (
-              <div className="post__video-container">
-                <video
-                  ref={videoRef}
-                  src={url}
-                  className="post-preview"
-                  autoPlay
-                  muted
-                  onEnded={() => {
-                    if (videoRef.current) {
-                      videoRef.current.pause();
-                      videoRef.current.currentTime = 0;
-                    }
-                  }}
-                />
-              </div>
-            ) : null}
-          </div>
-        ))}
+        {capturedImage ? (
+          <img
+            src={capturedImage}
+            className="post-preview"
+            alt="Captured Preview"
+          />
+        ) : (
+          fileUrls.map((url, index) => (
+            <div key={index} className="post__preview-item">
+              <button
+                className="remove-file-btn"
+                onClick={() => removeFile(index)}
+              >
+                &times;
+              </button>
+              {files[index]?.type.startsWith("image") ? (
+                <img src={url} className="post-preview" alt="Preview" />
+              ) : files[index]?.type.startsWith("video") ? (
+                <div className="post__video-container">
+                  <video
+                    ref={videoRef}
+                    src={url}
+                    className="post-preview"
+                    autoPlay
+                    muted
+                    onEnded={() => {
+                      if (videoRef.current) {
+                        videoRef.current.pause();
+                        videoRef.current.currentTime = 0;
+                      }
+                    }}
+                  />
+                </div>
+              ) : null}
+            </div>
+          ))
+        )}
       </div>
 
       {fileUrls.length > 0 && fileUrls.length < 4 && (
-  <Button
-    className="add-more-btn"
-    icon={<span className="anticon">+</span>}
-    onClick={addMoreFiles}
-  >
-    Select More
-  </Button>
-)}
-
-
+        <Button
+          className="add-more-btn"
+          icon={<span className="anticon">+</span>}
+          onClick={addMoreFiles}
+        >
+          Select More
+        </Button>
+      )}
+{fileUrls.length < 1 &&(
+      <Button
+        onClick={handleCameraClick}
+        className="post-btn"
+        style={{ width: 200 }}
+      >
+        Capture
+      </Button>
+)
+}
       <div className="post__caption-container">
         <p className="post-caption">Caption</p>
         <Input.TextArea
--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -5,15 +5,14 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 
-console.log(process.env.REACT_APP_FIREBASE_API_KEY);
 
-const API_KEY = process.env.REACT_APP_FIREBASE_API_KEY;
-const AUTH_DOMAIN = process.env.REACT_APP_FIREBASE_AUTH_DOMAIN;
-const PROJECT_ID = process.env.REACT_APP_FIREBASE_PROJECT_ID;
-const STORAGE_BUCKET = process.env.REACT_APP_FIREBASE_STORAGE_BUCKET;
-const MESSAGING_SENDER_ID = process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID;
-const APP_ID = process.env.REACT_APP_FIREBASE_APP_ID;
-const MEASUREMENT_ID = process.env.REACT_APP_FIREBASE_MEASUREMENT_ID;
+const API_KEY = import.meta.env.VITE_FIREBASE_API_KEY
+const AUTH_DOMAIN =import.meta.env.VITE_FIREBASE_AUTH_DOMAIN;
+const PROJECT_ID = import.meta.env.VITE_FIREBASE_PROJECT_ID;
+const STORAGE_BUCKET = import.meta.env.VITE_FIREBASE_STOARGE_BUCKET;
+const MESSAGING_SENDER_ID = import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID;
+const APP_ID = import.meta.env.VITE_FIREBASE_APP_ID;
+const MEASUREMENT_ID =import.meta.env.VITE_FIREBASE_MEASUREMENT_ID;
 
 const firebaseConfig = {
   apiKey: API_KEY,
